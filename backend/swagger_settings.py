e94f6052400b0127405d0c39e82f7b8473a221e1
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-02-09 20:28:44 +0100

    add swagger documentation

---
+++ backend/swagger_settings.py
@@ -0,0 +1,13 @@
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
