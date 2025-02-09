7eaa5d29d7289873b243b67863b8ad67969069c5
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-02-09 20:27:00 +0100

    add swagger documentation

--- backend/outils.py
+++ backend/outils.py
@@ -4,13 +4,13 @@ from data.data import POSTS
 def sort_posts(sort, direction):
     sorted_posts = POSTS
     if sort == "title":
-        sorted_posts = sorted(sorted_posts, key=lambda x: x["title"])
+        sorted_posts = sorted(sorted_posts, key=lambda x: x["title"].lower())
         if direction == "desc":
-            sorted_posts = sorted(sorted_posts, key=lambda x: x["title"], reverse=True)
+            sorted_posts = sorted(sorted_posts, key=lambda x: x["title"].lower(), reverse=True)
     if sort == "content":
-        sorted_posts = sorted(sorted_posts, key=lambda x: x["content"])
+        sorted_posts = sorted(sorted_posts, key=lambda x: x["content"].lower())
         if direction == "desc":
-            sorted_posts = sorted(sorted_posts, key=lambda x: x["content"], reverse=True)
+            sorted_posts = sorted(sorted_posts, key=lambda x: x["content"].lower(), reverse=True)
     return sorted_posts
 
 
